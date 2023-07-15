6cd1f48399ce810a9d16ae864a790ae49c67bc14
Mark Gorbunov <markgorbunov1995@mail.ru>
2023-07-15 15:30:51 +0200

    add simple tests

---
+++ src/test/java/SimpleTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class SimpleTests {
+    @Test
+    public void passedTest() {
+        assert 2 + 2 == 4;
+    }
+
+    @Test
+    public void failedTest() {
+        assert 2 + 2 == 5;
+    }
+}
